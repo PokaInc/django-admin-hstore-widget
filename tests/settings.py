53608a44eae9d43f6de589f60b6d488986fbfbd0
Alexandre Dufour <adufour@poka.io>
2018-03-05 15:20:46 -0500

    Basic test setup

---
+++ tests/settings.py
@@ -0,0 +1,15 @@
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': ':memory:',
+    },
+}
+
+INSTALLED_APPS = (
+    'tests',
+)
+
+USE_TZ = True
+
+SECRET_KEY = 'secret'
