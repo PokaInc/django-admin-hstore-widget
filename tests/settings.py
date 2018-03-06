04bcd65fbadab9b3e209357f23999c5cc26013da
Alexandre Dufour <adufour@poka.io>
2018-03-06 14:53:15 -0500

    Fix missing middleware to access request user in admin

--- tests/settings.py
+++ tests/settings.py
@@ -34,14 +34,15 @@ else:
                          'django.contrib.admin',
                      ) + INSTALLED_APPS
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
+]
 
 SECRET_KEY = 'secret'
 
