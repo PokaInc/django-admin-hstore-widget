476a955e09c83b10940511f618a2a49358d99ad0
Artem <temi4@users.noreply.github.com>
2019-12-05 10:33:33 +0300

    django 3 compatible

--- django_admin_hstore_widget/widgets.py
+++ django_admin_hstore_widget/widgets.py
@@ -1,5 +1,8 @@
 from django.conf import settings
-from django.contrib.admin.templatetags.admin_static import static
+try:
+    from django.contrib.admin.templatetags.admin_static import static
+except ModuleNotFoundError:
+    from django.templatetags.static import static
 from django.contrib.admin.widgets import AdminTextareaWidget
 from django.contrib.postgres.forms import forms
 from django.template.loader import get_template
