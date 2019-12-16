9b436c44b67de53b80fba166e61d6e076af6fca9
Alexandre Dufour <adufour@poka.io>
2019-12-16 09:03:04 -0500

    Add missing python

--- runtests.py
+++ runtests.py
@@ -2,10 +2,10 @@
 
 import os
 import sys
+from django.core.management import execute_from_command_line
 
 
 def runtests():
-    from django.core.management import execute_from_command_line
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tests.settings")
     argv = sys.argv[:1] + ['test'] + sys.argv[1:]
     execute_from_command_line(argv)
