1022ed92be11084b892ba1628c951a17c88447e7
Alexandre Dufour <adufour@poka.io>
2019-12-16 07:28:21 -0500

    ADd Python 3.7 and Django 3 in test matrix

--- django_admin_hstore_widget/widgets.py
+++ django_admin_hstore_widget/widgets.py
@@ -1,6 +1,6 @@
 try:
     from django.contrib.admin.templatetags.admin_static import static
-except ModuleNotFoundError:
+except ImportError:
     from django.templatetags.static import static
 from django.contrib.admin.widgets import AdminTextareaWidget
 from django.contrib.postgres.forms import forms
